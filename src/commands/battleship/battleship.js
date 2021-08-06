43b9cb897bd6735617723fbfb92e8b2ffe860313
Malachi Fraenkel <malachi.fraenkel@gmail.com>
2021-08-06 19:52:34 +0300

    added battleship sunk announcer

--- src/commands/battleship/battleship.js
+++ src/commands/battleship/battleship.js
@@ -6,8 +6,8 @@ class Battleship {
 		this.player1 = player1;
 		this.player2 = player2;
 		this.turn = Math.random() > 0.5 ? this.player1 : this.player2;
+		// 0-nothing, 1-ship middle, 2-left edge, 3-right edge, 4-top edge, 5-bottom edge, 6-hit (board), 7-miss (enemymap), 8-hit (enemymap).
 		this.board1 = [
-			// 0-nothing, 1-ship middle, 2-left edge, 3-right edge, 4-top edge, 5-bottom edge, 6-hit.
 			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -57,6 +57,8 @@ class Battleship {
 		];
 		this.score1 = 0;
 		this.score2 = 0;
+		this.shipLocations1 = [];
+		this.shipLocations2 = [];
 	}
 
 	generate(board) {
@@ -121,7 +123,11 @@ class Battleship {
 					row = Math.floor(Math.random() * 10);
 					col = Math.floor(Math.random() * 10);
 				}
-				placeHorizontal(board, row, testHorizontal(board, n, row, col)[1], n);
+				const newCol = testHorizontal(board, n, row, col)[1];
+				placeHorizontal(board, row, newCol, n);
+				board === this.board1
+					? this.shipLocations1.push(['horizontal', row, newCol, n])
+					: this.shipLocations2.push(['horizontal', row, newCol, n]);
 			}
 			if (alignment === 'vertical') {
 				let row = Math.floor(Math.random() * 10);
@@ -130,7 +136,11 @@ class Battleship {
 					row = Math.floor(Math.random() * 10);
 					col = Math.floor(Math.random() * 10);
 				}
-				placeVirtical(board, col, testVirtical(board, n, row, col)[1], n);
+				const newRow = testVirtical(board, n, row, col)[1];
+				placeVirtical(board, col, newRow, n);
+				board === this.board1
+					? this.shipLocations1.push(['vertical', newRow, col, n])
+					: this.shipLocations2.push(['vertical', newRow, col, n]);
 			}
 		};
 
@@ -152,8 +162,6 @@ class Battleship {
 	toString(board) {
 		let boardDisplay = clone(board);
 		for (let i = 0; i < boardDisplay.length; i++) {
-			//boardDisplay.push(board[i]);
-			//boardDisplay[i] = Object.assign([], board[i]);
 			if (i === 0) boardDisplay[i].unshift('🔟');
 			else if (i === 1) boardDisplay[i].unshift('9️⃣');
 			else if (i === 2) boardDisplay[i].unshift('8️⃣');
@@ -194,14 +202,49 @@ class Battleship {
 	shoot(row, col) {
 		let board = this.turn === this.player1 ? this.board2 : this.board1;
 		let enemyMap = this.turn === this.player1 ? this.enemyMap1 : this.enemyMap2;
+
+		const sunk = () => {
+			const shipLocations = this.turn === this.player1 ? this.shipLocations2 : this.shipLocations1;
+
+			for (let i = 0; i < shipLocations.length; i++) {
+				if (shipLocations[i][0] === 'horizontal') {
+					let counter = 0;
+					for (let j = 0; j < shipLocations[i][3]; j++) {
+						if (board[shipLocations[i][1]][shipLocations[i][2] + j] === 6) counter++;
+					}
+					if (counter === shipLocations[i][3]) {
+						shipLocations.splice(i, 1);
+						return true;
+					}
+				}
+				if (shipLocations[i][0] === 'vertical') {
+					let counter = 0;
+					for (let j = 0; j < shipLocations[i][3]; j++) {
+						if (board[shipLocations[i][1] + j][shipLocations[i][2]] === 6) counter++;
+					}
+					if (counter === shipLocations[i][3]) {
+						shipLocations.splice(i, 1);
+						return true;
+					}
+				}
+			}
+
+			return false;
+		};
+
 		if (enemyMap[row][col] === 7 || enemyMap[row][col] === 8) {
 			return [false, "You've already shot at that spot."];
 		} else if (board[row][col] >= 1 && board[row][col] <= 5) {
 			this.turn === this.player1 ? (this.enemyMap1[row][col] = 8) : (this.enemyMap2[row][col] = 8);
 			this.turn === this.player1 ? (this.board2[row][col] = 6) : (this.board1[row][col] = 6);
 			this.turn === this.player1 ? this.score1++ : this.score2++;
+			const didSink = sunk();
 			this.turn === this.player1 ? (this.turn = this.player2) : (this.turn = this.player1);
-			return [true, 'Hit!'];
+			if (didSink) {
+				return [true, 'sunk!'];
+			} else {
+				return [true, 'Hit!'];
+			}
 		} else if (board[row][col] === 0) {
 			this.turn === this.player1 ? (this.enemyMap1[row][col] = 7) : (this.enemyMap2[row][col] = 7);
 			this.turn === this.player1 ? (this.turn = this.player2) : (this.turn = this.player1);
@@ -210,8 +253,8 @@ class Battleship {
 	}
 
 	ended() {
-		if (this.score1 === 17) return this.player1;
-		if (this.score2 === 17) return this.player2;
+		if (this.shipLocations1.length === 0) return this.player2;
+		if (this.shipLocations2.length === 0) return this.player1;
 		return false;
 	}
 }
